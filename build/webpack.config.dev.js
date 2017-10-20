6336df51ff5f19a39d2d44b5bfec58774bb47535
vini123 <lichking_lin86@qq.com>
2017-10-20 13:28:42 +0800

    the first demo. you kan write .vue

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,35 @@
+const HtmlPlugin = require('html-webpack-plugin')
+
+const path = require('path')
+
+const merge = require('webpack-merge')
+
+var BaseWebpackConfig = require('./webpack.config.base.js')
+
+
+module.exports = merge(BaseWebpackConfig, {
+		entry:'./src/index.js',
+		output:{
+			path: path.resolve(__dirname, '../dist'),
+			filename:'./style/js/[name]-[hash].bundle.js'
+		},
+		module:{
+			rules:[
+				{
+					test:/\.vue$/,
+					use:{
+						loader:'vue-loader'
+					}
+				}
+			]
+		},
+		plugins:[
+			new HtmlPlugin({
+				filename:'home.html',
+				template:'./src/index.html',
+				title: 'this is home',
+				inject: 'body',
+				chunks:['main']
+			})
+		]
+})
---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+	<div>
+		<div class="u-name">{{name}} </div>
+		<span class="u-like" v-for="item in likes">
+			{{item}}
+		</span>
+	</div>
+</template>
+
+<script>
+	export default {
+		data(){
+			return {
+				name: '七月羽歌',
+				likes:[
+					'热血江湖',
+					'王者荣耀',
+					'跑跑卡丁车',
+					'地下城勇士'
+				]
+			}
+		}
+
+	}
+</script>
+
+<style>
+	.u-name{
+		color: #000;
+		font-size: 16px;
+		font-weight:bold;
+		margin-left: 5px;
+	}
+
+	.u-like{
+		display:inline-block;
+		padding: 6px 15px;
+		border-radius: 5px;
+		font-size: 13px;
+		color: #fff;
+		background-color: #a38;
+		cursor: pointer;
+		margin: 15px 5px;
+	}
+</style>
